14278bae53d4feaffcb8ecae9f44f8c361e26de2
Kamran <kamran@LP-PF1548CK.HCLT.CORP.HCL.IN>
2018-10-25 16:16:41 +0530

    fixed invite timeout issue

--- app/web/route.py
+++ app/web/route.py
@@ -2,16 +2,19 @@ import os
 import random
 import asyncio
 import aiosip
-from aiohttp import web
+import contextlib
 
+from aiohttp import web
 from web.config import config 
 from web.sip.util import Registration
-
 
 routes = web.RouteTableDef()
 
 proxy_host = 'sipproxy'
 proxy_port = 6100
+
+uas_host = 'sipuas'
+uas_port = 6200
 
 realm = 'XXXXXX'
 user = 'siprest'
@@ -24,23 +27,21 @@ async def home(request):
   return web.json_response({'status':'ok'})
 
 
-@routes.get('/register/{from_user}/{from_host}/{from_port}')
+@routes.get('/register/{from_user}')
 async def register(request):
   print('Register request')
   from_user = request.match_info['from_user']
-  from_host = request.match_info['from_host'] 
-  from_port = request.match_info['from_port'] 
 
   sip = aiosip.Application(loop=request.loop)
  
   local_port = random.randint(6001, 6999)
   peer = await sip.connect((proxy_host, proxy_port), protocol=aiosip.TCP, local_addr=(local_host, local_port))
+  
+  from_details = header(from_user,uas_host, uas_port)
+  to_details=header(from_user, uas_host, uas_port)
+  contact_details=header(from_user, uas_host, uas_port)
  
-  rdialog = await peer.register(
-        from_details=header(from_user,from_host, from_port), 
-        to_details=header(from_user, from_host, from_port),
-        contact_details=header(from_user, from_host, from_port),
-        password=pwd)
+  rdialog = await peer.register( from_details=from_details, to_details=to_details, contact_details=contact_details, password=pwd )
   await sip.close()
 
   print('Registration done')
@@ -59,7 +60,6 @@ async def invite(request):
   call = await peer.invite(
                from_details=header(user, local_host, local_port), 
                to_details=header(to_user, local_host, local_port),
-               contact_details=header(user, local_host, local_port),
                password=pwd)
 
   async with call:
